5dd449c4638d7af40886c0f4c8511ddcfecf7967
Laura <martinaslaura123@gmail.com>
2021-10-31 15:43:26 +0200

    added list.py

---
+++ list.py
@@ -0,0 +1,25 @@
+num=[1,2,3,4]
+print(num)
+
+letter=[ "a", "c", "f", "s"]
+print(letter)
+
+stg=["get","certified","before"]
+print(stg)
+
+mix=[ 1,4, "john", "maria"]
+print(mix)
+
+mat=[[1,2],["a", "b"]]
+print(mat)
+
+#accessing elements in lists
+
+print(mix)
+print(mix[3])
+print(mix[-2])
+print(mix[:2])
+print(mix[2:])
+print(mix[1:3])
+print(mix[::2])
+print(mix[::-1])
